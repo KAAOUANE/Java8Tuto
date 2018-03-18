0ad43add34aff0cedb7a9330d0e1dd0e6357bf39
mounir <mounirlyon@gmail.com>
2018-03-18 22:14:46 +0100

    Methode reference Java 8

--- src/test/java/MethodReferenceTest.java
+++ src/test/java/MethodReferenceTest.java
@@ -1,6 +1,12 @@
+import com.sun.deploy.util.StringUtils;
 import interfaceFonctionnel.Display;
 import org.junit.Test;
 import stream.Personne;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static stream.Personne.getStaticMethode;
 
 /**
  * Created by admin on 18/03/2018.
@@ -9,29 +15,44 @@ public class MethodReferenceTest {
 
     //Les methodes references peuvent etre utiliser comme alternative au expresion
     //lamnda pour simplifier la lecture du code
+    @Test
+    public void referenceStaticMethodeTest(){
+        Display display = Personne::getStaticMethode;
+        System.out.println(display.show());
+        System.out.println(Personne.getStaticMethode());
+    }
 
     @Test
-    public void referenceStaticMethodeFile(){
-        //Personne::getStaticMethode();
-
+    public void referenceInstanceMethodeTest(){
+        Personne p = new Personne("toto@toto.fr","toto","LYON",1);
+        Display display = p::getName;
+        System.out.println(display.show());
+        System.out.println(p.getName());
     }
+
+    interface Messageable{
+        Message getMessage(String msg);
+    }
+    class Message{
+        Message(String msg){
+            System.out.print(msg);
+        }
+    }
+
     @Test
-    public void referenceInstanceMethodeFile(){
-
-        //Personne p = new Personne("toto@toto.fr","toto","LYON",1);
-        //System.out.println(p::getName);
-
+    public void referenceConstructeurTest(){
+        Messageable hello = Message::new;
+        hello.getMessage("Hello");
     }
+
+
     @Test
-    public void referenceConstructeurFile(){
-        //Personne::new;
-
-
+    public void referenceType() {
+        System.out.println(Stream.of("", "a", "c").filter(String::isEmpty).count());
+        //System.out.println(Stream.of("","a","c").filter(String::!isEmpty).count());
+        System.out.println(Stream.of("", "a", "c").filter(s -> !s.isEmpty()).count());
+        System.out.println(Stream.of("", "a", "c").filter(((Predicate<String>) String::isEmpty).negate()).count());
+        Stream.of("", "a", "c").filter(((Predicate<String>) String::isEmpty).negate()).forEach(System.out::println);
     }
-    @Test
-    public void referenceType(){
-    //String::isEmpty
-    }
-    //System.out::println
 
 }
